123c5ccf7e482b700931267fcdf0d5f2b45b2ee6
Raky-S <rakysy1@gmail.com>
2020-09-16 17:50:35 +0200

    first commit

---
+++ first-test/src/components/Title.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Guess what am I. I have three horns! What's your answer? </h1>
+                  
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
