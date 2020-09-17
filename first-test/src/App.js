ec7de1d0c7162c80af2a7e7c671bca8b3f9be8db
Raky-S <rakysy1@gmail.com>
2020-09-17 14:57:17 +0200

    seconde

--- first-test/src/App.js
+++ first-test/src/App.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './components/Title.js';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <hi>Hello World! Hello!</hi>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Title></Title>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            
-          </a>
+          ></a>
         </header>
       </div>
     );
   }
 }
-
 export default App;
--- first-test/src/components/Title.js
+++ first-test/src/components/Title.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+/*
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1> Guess what am I. I have three horns! What's your answer? </h1>
-                  
-        </header>
+      <div prhase="App">
+        <h1>Hello World Konexio!</h1>
+        <h2> Name: {this.props.name}</h2>
       </div>
     );
   }
 }
 
-export default App;
+export default App;*/
+
+
+
+
+
+
+
+class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Guess what am I. I have three horns! What's your answer? </h2>
+        <h3> By the way , logo is going backwards and faster!</h3>
+        </div>
+    );
+  }
+}
+
+export default Title;
+
+
+// 1 sur konexio faire un dossier vide react
+// 2 dans cmder faire git init dans le react
+// 3 faire le npx changer le my app pour first-
+// 4 ouvrir le dossier enfantsur vscode
+// 5 faire un push
+
